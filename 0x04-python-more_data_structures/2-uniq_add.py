e4226c59210320b2f56aa8cc06b9b71ab2c9cb56
joehsn <youssef.hassan.mahmoud@gmail.com>
2023-12-06 14:52:54 -0800

    the 2 task didn't meet the requirments

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    my_set = set(())
-    for item in my_list:
-        my_set.add(item)
-    return len(my_set)
+    unique_set = set() 
+    result = 0
+
+    for num in my_list:
+        if num not in unique_set:
+            result += num
+            unique_set.add(num)
+
+    return result
