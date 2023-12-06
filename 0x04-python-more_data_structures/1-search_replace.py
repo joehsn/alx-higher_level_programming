7c9234712f04df0a9685624c81bf5b8997e1071c
joehsn <youssef.hassan.mahmoud@gmail.com>
2023-12-06 14:39:56 -0800

    the 1th task did not meet the requirments

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for idx in range(0, len(my_list)):
-        if my_list[idx] == search:
-            my_list[idx] = replace
+    new_list = []
+    for item in my_list:
+        if item == search:
+            new_list.append(replace)
+        else:
+            new_list.append(item)
+    return new_list
