be3f77c0fade44b3d7334dec12933e29b8a88487
Youssef Hassan <youssef.hassan.mahmoud@gmail.com>
2023-12-18 13:24:32 -0800

    pycodestyle was forgotten

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0;
+    count = 0
     try:
         for item in my_list:
             if count < x:
                 print(item, end="")
                 count += 1
         print()
-    except:
+    except Exception:
         pass
     return count
